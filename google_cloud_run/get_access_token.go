b78ca37eb55860714f2c985fa5de2111fe1bb4ae
Kanta Oikawa <k.oikawa@kantacky.com>
2024-07-05 00:20:33 +0900

    Updatea Google Cloud Run

--- google_cloud_run/get_access_token.go
+++ google_cloud_run/get_access_token.go
@@ -3,27 +3,26 @@ package google_cloud_run
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	"google.golang.org/api/idtoken"
 )
 
 func GetAccessToken(endpoint string) (*string, error) {
-	accessToken := os.Getenv("GOOGLE_CLOUD_RUN_ACCESS_TOKEN")
-	if accessToken != "" {
-		return &accessToken, nil
+	idToken := os.Getenv("GOOGLE_CLOUD_IDENTITY_TOKEN")
+	if idToken != "" {
+		return &idToken, nil
 	}
-		tokenSource, err := idtoken.NewTokenSource(context.Background(), endpoint)
-		if err != nil {
-			return nil, fmt.Errorf("idtoken.NewTokenSource error: %s", err)
-		}
 
-		token, err := tokenSource.Token()
-		if err != nil {
-			log.Println("tokenSource.Token error:", err)
-			return nil, fmt.Errorf("tokenSource.Token error: %s", err)
-		}
+	tokenSource, err := idtoken.NewTokenSource(context.Background(), endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("idtoken.NewTokenSource error: %w", err)
+	}
+
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, fmt.Errorf("tokenSource.Token error: %w", err)
+	}
 
 	return &token.AccessToken, nil
 }
